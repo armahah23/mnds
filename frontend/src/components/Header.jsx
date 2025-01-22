e63e05d97a993fd95da71e1add344fc9030d8a8a
A R M Afrih <151869356+armahah23@users.noreply.github.com>
2025-01-22 14:08:44 +0530

    Refactor Header component styles for improved responsiveness and visual hierarchy

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,14 +8,14 @@ function Header() {
           className="bg-gray-800 text-white py-8 h-[400px] flex justify-center items-center"
           style={{ backgroundImage: `url(${bg_container})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
         >
-          <div className="container mx-auto">
-            <h1 className="text-[48px] font-bold text-center main_header">
+          <div className="container mx-auto px-4">
+            <h1 className="text-3xl md:text-[48px] font-bold text-center main_header">
               Welcome to the <br />
-              <span className="bg-gradient-to-r from-orange-300 to-orange-600 text-transparent bg-clip-text text-8xl tracking-wider">
+              <span className="bg-gradient-to-r from-orange-300 to-orange-600 text-transparent bg-clip-text text-5xl md:text-8xl tracking-wider">
                 MndS
               </span>
             </h1>
-            <h2 className="text-center h2 tracking-wide">Movie and Web Series</h2>
+            <h2 className="text-center text-lg md:text-2xl tracking-wide">Movie and Web Series</h2>
           </div>
         </div>
       </div>
