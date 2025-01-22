e7e18ed8ecc4b3463f9fc012051ee185eb981baf
A R M Afrih <151869356+armahah23@users.noreply.github.com>
2025-01-22 10:51:57 +0530

    Remove unused Vite dependency files and add Header component to Home page

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <div>
+      Hi, I am the header
+    </div>
+  )
+}
+
+export default Header
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import Navbar from "../components/Navbar"
+import Header from "../components/Header"
 
 
 function Home() {
   return (
     <div>
       <Navbar />
+      {/* header desing */}
+      <Header />
     </div>
   )
 }
